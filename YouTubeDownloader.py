33cc27f62024797e606a0c0ded70fbdf5a9a8782
chisoftltd <chisoftltd@gmail.com>
2021-03-18 17:00:31 +0000

    Create Function to Start Downloading

--- YouTubeDownloader.py
+++ YouTubeDownloader.py
@@ -17,3 +17,14 @@ link = StringVar()
 
 Label(root, text = 'Paste link Here:', font = 'arial 15 bold').place(x = 160 , y = 60)
 link_enter = Enter(root, width = 70, textvariable = link).place(x = 32, y = 90)
+
+
+# Create Function to Start Downloading
+def Downloader():
+    url = YouTube(str(link.get()))
+    video = url.streams.first()
+    video.download()
+    Label(root, text = 'DOWNLOADED', font = 'arial 15').place(x = 180, y = 210)
+
+Button(root, text = 'DOWNLOAD', font = 'arial 15 bold' , bg = 'pale violet red', padx = 2, command = Downloader).place(x = 180, y = 150)
+root.mainloop()

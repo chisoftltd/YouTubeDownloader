b163eaa2772302a3ed4b3d1d80c70b4ae9bb4446
chisoftltd <chisoftltd@gmail.com>
2021-03-19 00:02:23 +0100

    Python YouTube Video Downloader is an application to download videos from YouTube

--- YouTubeDownloader.py
+++ YouTubeDownloader.py
@@ -15,8 +15,8 @@ Label(root, text = 'Youtube Video Downloader', font = 'arial 20 bold').pack()
 # Create Field to Enter Link
 link = StringVar()
 
-Label(root, text = 'Paste link Here:', font = 'arial 15 bold').place(x = 160 , y = 60)
-link_enter = Entry(root, width = 70, textvariable = link).place(x = 32, y = 90)
+Label(root, text = 'Paste link Here:', font = 'arial 15 bold').pack()#.place(x = 200 , y = 60)
+link_enter = Entry(root, width = 70, textvariable = link).pack()#.place(x = 60, y = 90)
 
 
 # Create Function to Start Downloading
@@ -24,7 +24,7 @@ def Downloader():
     url = YouTube(str(link.get()))
     video = url.streams.first()
     video.download()
-    Label(root, text = 'DOWNLOADED', font = 'arial 15').place(x = 180, y = 210)
+    Label(root, text = 'DOWNLOADED', font = 'arial 15').pack()#.place(x = 180, y = 210)
 
 Button(root, text = 'DOWNLOAD', font = 'arial 15 bold' , bg = 'pale violet red', padx = 2, command = Downloader).place(x = 180, y = 150)
 root.mainloop()
